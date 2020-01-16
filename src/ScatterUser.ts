bead621624fc4385153d97b6c1c95120b92d1fae
Jon La Marr <jon.lamarr-ext@block.one>
2020-01-16 14:36:34 -0500

    Bump dependencies. Update to eosjs 21 RC1. Fix import issue

--- src/ScatterUser.ts
+++ src/ScatterUser.ts
@@ -1,6 +1,7 @@
 import { Api, JsonRpc } from 'eosjs'
 import { ec as EC } from 'elliptic'
 import { Signature, PublicKey } from 'eosjs/dist/eosjs-jssig'
+import { KeyType } from 'eosjs/dist/eosjs-numeric'
 import { Chain, SignTransactionResponse, UALErrorType, User } from 'universal-authenticator-library'
 import { UALScatterError } from './UALScatterError'
 
@@ -132,6 +133,6 @@ export class ScatterUser extends User {
         'hex'
       )
     const ellipticPublicKey = ec.keyFromPublic(ellipticRecoveredPublicKey)
-    return PublicKey.fromElliptic(ellipticPublicKey).toString()
+    return PublicKey.fromElliptic(ellipticPublicKey, KeyType.k1).toString()
   }
 }
