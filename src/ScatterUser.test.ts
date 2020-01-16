33e1d6b23a0cd517e26924cb1faf9e964497f750
Jon La Marr <jon.lamarr-ext@block.one>
2020-01-16 14:42:27 -0500

    Fix tests

--- src/ScatterUser.test.ts
+++ src/ScatterUser.test.ts
@@ -2,6 +2,7 @@ import { ec as EC } from 'elliptic'
 import { Chain, RpcEndpoint, UALError, UALErrorType } from 'universal-authenticator-library'
 import { ScatterUser } from './ScatterUser'
 import { Signature, PrivateKey } from 'eosjs/dist/eosjs-jssig'
+import { KeyType } from 'eosjs/dist/eosjs-numeric'
 
 const endpoint: RpcEndpoint = {
   protocol: 'https',
@@ -133,7 +134,7 @@ describe('ScatterUser', () => {
       const ellipticHashedString = ec.hash().update(dataAsString).digest()
 
       const ellipticSig = KPrivElliptic.sign(ellipticHashedString)
-      const ellipticSigString = Signature.fromElliptic(ellipticSig).toString()
+      const ellipticSigString = Signature.fromElliptic(ellipticSig, KeyType.k1).toString()
 
       const eosioPubKey = user.getPublicKey(dataAsString, ellipticSigString)
       expect(eosioPubKey.toString()).toEqual(publicKeys[0])
@@ -148,7 +149,7 @@ describe('ScatterUser', () => {
       const ellipticHashedString = ec.hash().update(dataAsString).digest()
 
       const ellipticSig = KPrivElliptic.sign(ellipticHashedString)
-      const ellipticSigString = Signature.fromElliptic(ellipticSig).toString()
+      const ellipticSigString = Signature.fromElliptic(ellipticSig, KeyType.k1).toString()
 
       const eosioPubKey = user.getPublicKey('other string', ellipticSigString)
       expect(eosioPubKey.toString()).not.toEqual(publicKeys[0])
